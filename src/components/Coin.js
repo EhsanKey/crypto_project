b44b96251ff1fb436dcff0430141c15dbb5c7169
Ehsan Keyhani <ehsankeyhani82@gmail.com>
2022-04-29 12:11:03 -0700

    add style responsive

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -7,9 +7,13 @@ const Coin = ({name, image, symbol, price, marketCap, priceChange}) => {
 
     return (
         <div className={Styles.container}>
-            <img  className={Styles.image} src={image} alt={name} /> 
-            <span className={Styles.name}>{name}</span>
-            <span className={Styles.symbol}>{symbol.toUpperCase()}</span>
+            <div className={Styles.deviceMobail}>
+                <img  className={Styles.image} src={image} alt={name} /> 
+                <div>
+                    <span className={Styles.name}>{name}</span>
+                    <span className={Styles.symbol}>{symbol.toUpperCase()}</span>
+                </div>
+            </div>
             <span className={Styles.currentPrice}>$ {price.toLocaleString()}</span>
             <span className={priceChange > 0 ? Styles.greenPriceChange : Styles.redPriceChange}>{priceChange.toFixed(2)}%</span>
             <span className={Styles.marketCap}>$ {marketCap.toLocaleString()}</span>
